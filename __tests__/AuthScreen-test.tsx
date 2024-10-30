fb4cc24bb252e680697e146c050805fef5220789
Abhishek Sharma <abhisheksharmayt2@gmail.com>
2024-10-31 02:08:29 +0530

    test(AuthScreen): updated "sign with github" test case

--- __tests__/AuthScreen-test.tsx
+++ __tests__/AuthScreen-test.tsx
@@ -6,6 +6,7 @@ import { Toast } from 'react-native-toast-message/lib/src/Toast';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { customRenderWithProvider } from '../src/utils/tests';
+import { Linking } from 'react-native';
 
 jest.mock('react-redux', () => {
   return {
@@ -14,22 +15,24 @@ jest.mock('react-redux', () => {
   };
 });
 
+jest.mock('react-native/Libraries/Linking/Linking', () => ({
+  openURL: jest.fn(() => Promise.resolve('mockResolve')),
+  getInitialURL: jest.fn(() => Promise.resolve('mockResolve')),
+  addEventListener: jest.fn(),
+}));
+
 it('AuthScreen is rendered', () => {
   customRenderWithProvider(AuthScreen);
   screen.getByText(/welcome to/i);
   screen.getByText(/real dev squad/i);
 });
 
-it.skip('Clicking on Sign in with Github shows a toast', async () => {
-  render(
-    <>
-      <AuthScreen />
-      <Toast />
-    </>,
-  );
+it('Clicking on Sign in with Github opens browser', async () => {
+  customRenderWithProvider(AuthScreen);
+
   const githubSignInBtn = screen.getByText(Strings.SIGN_IN_BUTTON_TEXT);
   fireEvent.press(githubSignInBtn);
-  screen.getByText(/Sign in with GitHub coming soon/i);
+  expect(Linking.openURL).toHaveBeenCalledTimes(1);
 });
 
 describe('AuthScreen', () => {
