328dcbe3ef10ce27a590be82e0569301a40a1ebc
Aditi Shinde <aaditishinde1804@gmail.com>
2025-04-01 17:06:23 +0530

    basic project setup & AuthScreen migration

--- RDSExpoApp/app/(tabs)/NotifyScreen.tsx
+++ RDSExpoApp/app/(tabs)/NotifyScreen.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Notify Screen</Text>
+      <Text style={styles.title}>Notification Screen</Text>
     </View>
   );
 }
