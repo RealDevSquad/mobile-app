9d3ba9f842539570e23f8f7279157fb989b609b5
yash raj <yashraj0207@outlook.com>
2025-05-03 01:53:18 +0530

    add profile and not found route

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +10,21 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Welcome to RDS mobile app</Text>
+      <Text style={styles.text}>Welcome to RDS mobile app</Text>
+
+      <Link href="/profile" style={styles.button}>
+        Go to Profile screen
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    paddingBottom: 10,
+  },
+  button: {
+    fontSize: 12,
+    textDecorationLine: "underline",
+  },
+});
