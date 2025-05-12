4635b3715a987d4aeb4ba0fe654f93a641123ee1
Aditi Shinde <100122761+Aditys018@users.noreply.github.com>
2025-05-12 23:32:11 +0530

    Environment setup for the Expo App (#515)
    * initial commit
    
    * fix ts errors
    
    * udpate readme
    
    * add profile and not found route
    
    * use tabs
    
    * .env impl
    
    * dev,staging and prod. environment setup
    
    * Comments resolved
    
    * comments resolved gitignore , removed logs
    
    * modified CI/CD automated Environment setup
    
    * .tsconf
    
    ---------
    
    Co-authored-by: yash raj <yashraj0207@outlook.com>

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,15 @@
+import { isDev } from "@/constants/utils";
 import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Text style={styles.text}>Welcome to RDS mobile app</Text>
+
+      {isDev() && (
+        <Text style={styles.text}>This is the development version</Text>
+      )}
 
       <Link href="/profile" style={styles.button}>
         Go to Profile screen
@@ -20,10 +19,18 @@ export default function Index() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   text: {
     paddingBottom: 10,
+    fontSize: 18,
+    fontWeight: "bold",
   },
   button: {
+    marginTop: 10,
     fontSize: 12,
     textDecorationLine: "underline",
   },
